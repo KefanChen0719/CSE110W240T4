d7750ff32c2caf51d7fb76777f0648dc2c537b2a
Jiasheng Zhu <gnehsaijuhz@hotmail.com>
2016-02-24 16:02:29 -0800

    scroll view in prossessing

---
+++ TeamUp/TeamUp/myGroupsViewController.h
@@ -0,0 +1,16 @@
+//
+//  myGroupsViewController.h
+//  TeamUp
+//
+//  Created by Jiasheng Zhu on 2/24/16.
+//  Copyright © 2016 CSE110W240T4. All rights reserved.
+//
+
+#ifndef myGroupsViewController_h
+#define myGroupsViewController_h
+#import <UIKit/UIKit.h>
+@interface myGroupsViewController : UIViewController
+
+@end
+
+#endif /* myGroupsViewController_h */
