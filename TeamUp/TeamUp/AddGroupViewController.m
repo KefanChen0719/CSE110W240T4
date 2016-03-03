8358ef5747e36f104feeb28a4c87248d715b848e
Ryan <xil248@ucsd.edu>
2016-03-03 10:31:21 -0800

    AddGroup scene

---
+++ TeamUp/TeamUp/AddGroupViewController.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+AddGroupViewController.m
+//  TeamUp
+//
+//  Created by Ryan Li on 3/3/16.
+//  Copyright © 2016 CSE110W240T4. All rights reserved.
+//
+
+#import "AddGroupViewController.h"
+
+@implementation UIViewController (AddGroupViewController)
+
+@end
