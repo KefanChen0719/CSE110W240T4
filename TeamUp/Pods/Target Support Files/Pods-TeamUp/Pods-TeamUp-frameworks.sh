71aeac5d3461f837d5687025451e1c89ebc10681
renoyin <renoyin9536@gmail.com>
2016-03-06 22:59:04 -0800

    update cocoa pods

--- TeamUp/Pods/Target Support Files/Pods-TeamUp/Pods-TeamUp-frameworks.sh
+++ TeamUp/Pods/Target Support Files/Pods-TeamUp/Pods-TeamUp-frameworks.sh
@@ -82,3 +82,14 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-TeamUp/Firebase.framework"
+  install_framework "Pods-TeamUp/JSQMessagesViewController.framework"
+  install_framework "Pods-TeamUp/JSQSystemSoundPlayer.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-TeamUp/Firebase.framework"
+  install_framework "Pods-TeamUp/JSQMessagesViewController.framework"
+  install_framework "Pods-TeamUp/JSQSystemSoundPlayer.framework"
+fi
