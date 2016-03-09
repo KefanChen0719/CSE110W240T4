08ece66004c70ca9344376a3d8cb3b6a28361b65
Suli Huang <sulihuang@Sulis-MacBook-Pro.local>
2016-03-08 21:54:04 -0800

    update

---
+++ TeamUp/TeamUp/groupMemberDetails.h
@@ -0,0 +1,22 @@
+//
+//  Header.h
+//  TeamUp
+//
+//  Created by Suli Huang on 3/8/16.
+//  Copyright © 2016 CSE110W240T4. All rights reserved.
+//
+
+#ifndef groupMemberDetails_h
+#define groupMemberDetails_h
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+#import "TeamUp-Swift.h"
+@interface groupMemberDetails : UIViewController
+@property (strong, nonatomic) AppDelegate *appDelegate;
+@property (strong, nonatomic) UIViewController *viewcontroller;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *majorLabel;
+@property (weak, nonatomic) IBOutlet UILabel *yearLabel;
+
+@end
+#endif 
---
+++ TeamUp/TeamUp/groupMemberDetails.m
@@ -0,0 +1,22 @@
+//
+//  groupMemberDetails.m
+//  TeamUp
+//
+//  Created by Suli Huang on 3/8/16.
+//  Copyright © 2016 CSE110W240T4. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "groupMemberDetails.h"
+@interface groupMemberDetails ()
+
+@end
+
+@implementation groupMemberDetails
+@synthesize appDelegate,viewcontroller;
+
+-(void)viewDidLoad{
+    _nameLabel.text = @"hello";
+}
+
+@end
