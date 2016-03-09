ab3d1fdb33edd6d0c6139773f5c16bc32bb796f4
Jiasheng Zhu <gnehsaijuhz@hotmail.com>
2016-03-08 17:26:11 -0800

    fix bug

--- TeamUp/TeamUp/MemberDetailViewController.m
+++ TeamUp/TeamUp/MemberDetailViewController.m
@@ -16,25 +16,9 @@
 @synthesize appDelegate,viewcontroller;
 
 - (void)viewDidLoad {
-    NSString* QR_UID = @"";
+    __block NSString* QR_UID = @"";
     appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
-    UIScrollView *TeamMemberScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height*0.1, self.view.frame.size.width, self.view.frame.size.height*0.8)];
-    [TeamMemberScrollView setContentSize:CGSizeMake(TeamMemberScrollView.bounds.size.width, TeamMemberScrollView.bounds.size.height*3)];
-    CGRect contentRect = CGRectZero;
-    if([appDelegate.firebase.authData.uid isEqualToString:@""]){
-        QR_UID = @"ERROR";
-    }
-    else{
-        QR_UID = [appDelegate.currentClassUid stringByAppendingString:@";"];
-        QR_UID = [QR_UID stringByAppendingString:appDelegate.currentGroupUid];
-    }
-    CGFloat imageSize = ceilf(self.view.bounds.size.width * 0.6f);
-//    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(floorf(self.view.bounds.size.width * 0.5f - imageSize * 0.5f), floorf(self.view.bounds.size.height * 0.5f - imageSize * 0.5f), imageSize, imageSize)];
-    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(floorf(self.view.bounds.size.width * 0.5f - imageSize * 0.5f), floorf(0), imageSize, imageSize)];
-    imageView.image = [UIImage mdQRCodeForString:QR_UID size:imageView.bounds.size.width fillColor:[UIColor darkGrayColor]];
-    [TeamMemberScrollView addSubview:imageView];
-    contentRect = CGRectUnion(contentRect, imageView.frame);
-    TeamMemberScrollView.contentSize = contentRect.size;
+
 
     if(appDelegate.currentGroupUid && ![appDelegate.currentGroupUid isEqualToString:@""]){
         Firebase *curr_group = [appDelegate.firebase childByAppendingPath:@"classes"];
@@ -44,10 +28,24 @@
         curr_group = [curr_group childByAppendingPath:@"teammember"];
         [curr_group observeSingleEventOfType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
             NSMutableArray<NSString *> *member = snapshot.value;
+            //__block NSString* memberName = @"";
+            if([appDelegate.firebase.authData.uid isEqualToString:@""]){
+                QR_UID = @"ERROR";
+            }
+            else{
+                QR_UID = [appDelegate.currentClassUid stringByAppendingString:@";"];
+                QR_UID = [QR_UID stringByAppendingString:appDelegate.currentGroupUid];
+            }
+            CGFloat imageSize = ceilf(self.view.bounds.size.width * 0.6f);
+            UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(floorf(self.view.bounds.size.width * 0.5f - imageSize * 0.5f), floorf(0), imageSize, imageSize)];
+            imageView.image = [UIImage mdQRCodeForString:QR_UID size:imageView.bounds.size.width fillColor:[UIColor darkGrayColor]];
+            UIScrollView *TeamMemberScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height*0.1, self.view.frame.size.width, self.view.frame.size.height*0.8)];
+            [TeamMemberScrollView setContentSize:CGSizeMake(TeamMemberScrollView.bounds.size.width,  member.count * self.view.frame.size.height*0.1 + imageSize)];
             CGRect contentRect = CGRectZero;
+            [TeamMemberScrollView addSubview:imageView];
+            contentRect = CGRectUnion(contentRect, imageView.frame);
             for (NSInteger index = 0; index < member.count; index++)
             {
-                //__block NSString* memberName = @"";
                 __block UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
                 Firebase* curr_user_name = [appDelegate.firebase childByAppendingPath:@"users"];
                 curr_user_name = [curr_user_name childByAppendingPath: member[index]];
@@ -55,7 +53,7 @@
                     button.frame = CGRectMake(0, imageSize + self.view.frame.size.height*0.1 * (CGFloat)index, self.view.frame.size.width,self.view.frame.size.height*0.1);
                     [button setBackgroundColor:[UIColor colorWithRed:229.0/255.0 green:247.0/255.0 blue:248.0/255.0 alpha:1]];
                     button.tag = index;
-                    [button setTitle:[NSString stringWithFormat:snapshot.value[@"name"]] forState:UIControlStateNormal];
+                    [button setTitle:[NSString stringWithFormat:snapshot.value[@"name"], ((long)index + 1)] forState:UIControlStateNormal];
                     [button.titleLabel setFont:[UIFont systemFontOfSize:20]];
                     [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
                     [[button layer] setBorderWidth:2.0f];
@@ -64,11 +62,10 @@
                 [TeamMemberScrollView addSubview:button];
                 contentRect = CGRectUnion(contentRect, button.frame);
             }
-            TeamMemberScrollView.contentSize = contentRect.size;
+            [self.view addSubview:TeamMemberScrollView];
         }];
         
     }
-    [self.view addSubview:TeamMemberScrollView];
 }
 
 - (IBAction)quitGroup:(id)sender {
