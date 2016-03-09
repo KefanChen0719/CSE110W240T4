e779bd339c63fd5dd614560a04875547ad780894
Ryan <xil248@ucsd.edu>
2016-03-08 22:23:12 -0800

    update

--- TeamUp/TeamUp/groupMemberViewController.m
+++ TeamUp/TeamUp/groupMemberViewController.m
@@ -16,7 +16,7 @@
 
 @implementation groupMemberViewController
 @synthesize appDelegate,viewcontroller;
-
+NSArray<NSString*> *members_uid;
 - (void)viewDidLoad {
 //    __block NSString* QR_UID = @"";
     appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
@@ -36,6 +36,7 @@
         curr_group = [curr_group childByAppendingPath:@"teammember"];
         [curr_group observeSingleEventOfType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
             NSMutableArray<NSString *> *member = snapshot.value;
+            members_uid = member;
             UIScrollView *TeamMemberScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height*0.1, self.view.frame.size.width, self.view.frame.size.height*0.8)];
             [TeamMemberScrollView setContentSize:CGSizeMake(TeamMemberScrollView.bounds.size.width,  member.count * self.view.frame.size.height*0.1)];
             
@@ -45,7 +46,7 @@
                             __block UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
                             Firebase* curr_user_name = [appDelegate.firebase childByAppendingPath:@"users"];
                             curr_user_name = [curr_user_name childByAppendingPath: member[index]];
-                            appDelegate.member_uid = member[index];
+                           
                             [curr_user_name observeSingleEventOfType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
                                 button.frame = CGRectMake(0,self.view.frame.size.height*0.1 * (CGFloat)index, self.view.frame.size.width,self.view.frame.size.height*0.1);
                                 [button setBackgroundColor:[UIColor colorWithRed:229.0/255.0 green:247.0/255.0 blue:248.0/255.0 alpha:1]];
@@ -55,6 +56,7 @@
                                 [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
                                 [[button layer] setBorderWidth:2.0f];
                                 button.layer.borderColor = [[UIColor colorWithRed:219.0/255.0 green:237.0/255.0 blue:238.0/255.0 alpha:1] CGColor];
+                                [button addTarget:self action:@selector(didTapButton:) forControlEvents:UIControlEventTouchUpInside];
                                 [TeamMemberScrollView addSubview:button];
                             }];
                         }
@@ -63,6 +65,15 @@
         
     }
 }
+
+- (void)didTapButton:(UIButton *)button
+{
+    //NSLog(@"Button %ld %@", (long)button.tag, groups[groups_uid[button.tag]]);
+    viewcontroller = [appDelegate.storyboard instantiateViewControllerWithIdentifier:@"groupmemberDetailsViewController"];
+    appDelegate.member_uid = members_uid[button.tag];
+    [self presentViewController:viewcontroller animated:YES completion:nil];
+}
+
 
 
 
