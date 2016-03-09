d905dcb7aabff00391bafbea53f66e94ae46ded7
renoyin <renoyin9536@gmail.com>
2016-03-09 10:31:20 -0800

    fix chat bug

--- TeamUp/Pods/JSQMessagesViewController/JSQMessagesViewController/Layout/JSQMessagesCollectionViewFlowLayout.m
+++ TeamUp/Pods/JSQMessagesViewController/JSQMessagesViewController/Layout/JSQMessagesCollectionViewFlowLayout.m
@@ -71,7 +71,7 @@ const CGFloat kJSQMessagesCollectionViewCellLabelHeightDefault = 20.0f;
 - (void)jsq_configureFlowLayout
 {
     self.scrollDirection = UICollectionViewScrollDirectionVertical;
-    self.sectionInset = UIEdgeInsetsMake(10.0f, 4.0f, 10.0f, 4.0f);
+    self.sectionInset = UIEdgeInsetsMake(75.0f, 4.0f, 10.0f, 4.0f);
     self.minimumLineSpacing = 4.0f;
    
     _messageBubbleSizes = [NSMutableDictionary new];
